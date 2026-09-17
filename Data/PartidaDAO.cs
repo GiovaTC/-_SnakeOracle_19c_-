a44d190a050d7e76156600caf1f86ee0489c7ed2
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-09-17 16:06:46 -0500

    commit - PartidaRanking .

--- Data/PartidaDAO.cs
+++ Data/PartidaDAO.cs
@@ -67,10 +67,103 @@ namespace SnakeOracle19C.Data
             commandInsertar.Parameters.Add(parametroId);
             commandInsertar.ExecuteNonQuery();
 
-            commandInsertar.ExecuteNonQuery();
-
             return Convert.ToInt32(parametroId.Value.ToString());
+        }
 
+        public void GuardarPrtida(Partida partida)
+        {
+            using OracleConnection connection = conexion.obtenerConexion();
+            connection.Open();
+
+            string sql = @"
+                INSERT INTO PARTIDAS 
+                (
+                    ID_JUGADOR, 
+                    PUNTUACION, 
+                    LONGITUD_SERPIENTE, 
+                    RESULTADO, 
+                    FECHA_PARTIDA
+                )
+                VALUES 
+                (   :ID_JUGADOR, 
+                    :PUNTUACION, 
+                    :LONGITUD_SERPIENTE, 
+                    :RESULTADO, 
+                    SYSDATE
+                )";
+
+            using OracleCommand command =
+                new OracleCommand(sql, connection);
+
+            command.Parameters.Add(
+                ":ID_JUGADOR",
+                OracleDbType.Int32
+                ).Value = partida.IdJugador;
+
+            command.Parameters.Add(
+                ":PUNTUACION",
+                OracleDbType.Int32
+                ).Value = partida.Puntuacion;
+            
+            command.Parameters.Add(
+                ":LONGITUD",
+                OracleDbType.Int32
+                ).Value = partida.LongitudSerpiente;
+            
+            command.Parameters.Add(
+                ":RESULTADO",
+                OracleDbType.Varchar2
+                ).Value = partida.Resultado;
+            
+            command.ExecuteNonQuery();
+        }
+
+        public List<PartidaRanking> ObtenerRanking()
+        {
+            List<PartidaRanking> lista = new();
+
+            using OracleConnection connection = conexion.obtenerConexion();
+            connection.Open();
+
+            string sql = @"
+                SELECT *
+                FROM
+                (
+                    SELECT
+                        J.NOMBRE,
+                        P.PUNTUACION,
+                        P.LONGITUD_SERPIENTE,
+                        P.RESULTADO,
+                        P.FECHA_PARTIDA
+                    FROM PARTIDAS P
+                    INNER JOIN JUGADORES J
+                        ON J.ID_JUGADOR = P.ID_JUGADOR
+                    ORDER BY P.PUNTUACION DESC
+                )
+                WHERE ROWNUM <= 10";
+
+            using OracleCommand command = 
+                new OracleCommand(sql, connection);
+
+            using OracleDataReader reader = 
+                command.ExecuteReader();
+            
+            while (reader.Read())
+            {
+                lista.Add(new PartidaRanking
+                {
+                    Nombre = reader["NOMBRE"].ToString() ?? "",
+                    Puntuacion = Convert.ToInt32(reader["PUNTUACION"]),
+                    Longitud = Convert.ToUInt32(
+                        reader["LONGITUD_SERPIENTE"]),
+                    Resultado = reader["RESULTADO"].ToString() ?? "",
+                    Fecha = Convert.ToDateTime(
+                        reader["FECHA_PARTIDA"])
+                });
+            }
+
+            return lista;
+            
         }
     }
 }
---
+++ Data/PartidaRanking.cs
@@ -0,0 +1,11 @@
+﻿namespace SnakeOracle19C.Data
+{
+    public class PartidaRanking
+    {
+        public string Nombre { get; set; } = "";
+        public int Puntuacion { get; set; }
+        public uint Longitud { get; set; }
+        public string Resultado { get; set; } = "";
+        public DateTime Fecha { get; set; }
+    }
+}
