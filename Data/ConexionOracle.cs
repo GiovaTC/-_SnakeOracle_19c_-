2009ad00bd704f26caa9b470c536faa46c402022
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-09-17 15:15:37 -0500

    commit - ConexionOracle .

--- Data/ConexionOracle.cs
+++ Data/ConexionOracle.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SnakeOracle19C.Data
 {
-    internal class ConexionOracle
+    public class ConexionOracle
     {
-    }
+        private readonly string connectionString =
+            "User Id=system;" +
+            "Password=Tapiero123;" +
+            "Data Source=localhost:1521/orcl;"; 
+
+        public OracleConnection obtenerConexion()
+        {
+            return new OracleConnection(connectionString);
+        }
+    }   
 }
